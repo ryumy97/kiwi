a470723a3042fb2e5d69a00c02bf25bfa54cce46
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2022-01-14 02:29:03 +1300

    use my name

--- src/components/buttons/twitterButton.js
+++ src/components/buttons/twitterButton.js
@@ -4,10 +4,8 @@ export class twitterButton {
         this.element.id = 'share-tw';
         this.element.className = 'btn tw';
 
-        
-
         this.element.onclick = () => {
-            const description = 'Kiwi - a mini project by Ryumy97'
+            const description = 'Kiwi - a mini project by In Ha Ryu'
             const url = `https://kiwi.ryumy.com/${window.location.hash}`
             window.open(`https://twitter.com/intent/tweet?text=${description}&url=${url}`, '_blank', 'location=yes,height=570,width=520')
         };
