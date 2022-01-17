fd7fefe3d83ad31a52bbeb4b35900bedcc6ab8c9
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2022-01-18 04:15:14 +1300

    add break

--- src/components/aboutOverlay.js
+++ src/components/aboutOverlay.js
@@ -50,6 +50,7 @@ export class AboutOverlay {
                     The word <strong>Kiwi</strong> is an interesting word as it holds different meaning. A bird, a fruit or a New Zealander.
                     The kiwi is expressed in single form of circle or a ball which user can grab and move around in a physical environment.
                     As kiwi represents the several meaning, so the aim was to create simple container with different patterns of colors and drawings.
+                    <br>
                 </p>
                 <p class="textAppear9">
                     If you want to contact me, check out my<br>
