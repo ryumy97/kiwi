abb59417ed6efec7b21180cbe26468276c994ebe
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2022-01-17 18:20:53 +1300

    bugfix disable click when about overlay is transitioning

--- src/components/textButtons/aboutButton.js
+++ src/components/textButtons/aboutButton.js
@@ -1,5 +1,7 @@
 export class AboutButton {
     constructor() {
+        this.overlayRendering = false;
+
         this.element = document.createElement('li');
         this.element.id = 'about';
         this.element.className = 'primary';
@@ -9,19 +11,33 @@ export class AboutButton {
         aboutText.innerText = 'about this project';
 
         this.isOverlayOn = window.location.hash.includes('/about');
+        this.overlayRendering = this.isOverlayOn;
 
-        this.element.onclick = this.dispatchAboutOverlayEvent.bind(this)
+        this.element.onclick = this.dispatchAboutOverlayEvent.bind(this);
+
+        window.addEventListener('_about_addFinish', this.finishRendering.bind(this), false)
+        window.addEventListener('_about_removeFinish', this.finishRendering.bind(this), false)
 
         this.element.appendChild(aboutText);
     }
 
+    finishRendering() {
+        this.overlayRendering = false;
+        console.log('yes');
+    }
+
     dispatchAboutOverlayEvent() {
+        if (this.overlayRendering) {
+            return
+        }
+
         const event = new CustomEvent('_about', {
             detail: {
                 turnOverlayOn: !this.isOverlayOn
             }
         });
-
+        
+        this.overlayRendering = true;
         window.dispatchEvent(event);
         
         if (!this.isOverlayOn) {
@@ -33,7 +49,7 @@ export class AboutButton {
             const href = window.location.href;
             const replacement = href.replace('/about', '');
 
-            location.replace(replacement)
+            location.replace(replacement);
         }
 
         this.isOverlayOn = !this.isOverlayOn;
